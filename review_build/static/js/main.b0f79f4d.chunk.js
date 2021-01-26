dafc3366bcea7a8e55b6516e3e33103fff2244a1
Viktor Kosanyak <kosanyakviktor@gmail.com>
2021-01-26 17:44:14 +0200

    commit-one

---
+++ review_build/static/js/main.b0f79f4d.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonptodo-list"]=this["webpackJsonptodo-list"]||[]).push([[0],{10:function(t,o,s){},4:function(t,o,s){"use strict";s.r(o);var e=s(2),c=(s(1),s(3)),i=s.n(c),n=(s(10),document.querySelector("#root")),r=Object(e.jsx)("h1",{class:"title",children:"Todo List"});i.a.render(r,n)}},[[4,1,2]]]);
+//# sourceMappingURL=main.b0f79f4d.chunk.js.map
